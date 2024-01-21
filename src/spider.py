63ce9f04047b24a33899b565a64c82a73293ea6d
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-01-21 15:40:26 +0700

    add code for input data to postgres database

--- src/spider.py
+++ src/spider.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 from amazoncaptcha import AmazonCaptcha
 import time
 import openpyxl
+from sqlalchemy import text
+from sqlalchemy import engine
+import psycopg2
+import os
 
 
 class AmazonScraper:
@@ -27,7 +31,10 @@ class AmazonScraper:
             return int(max_page.text)
         
         button_pagination = soup.find_all('a', class_='s-pagination-item s-pagination-button')
-        print('button pagination', button_pagination)
+        
+        if len(button_pagination) == 0:
+            return 1
+
         max_page = button_pagination[-1].text
 
         return int(max_page)
@@ -46,17 +53,17 @@ class AmazonScraper:
             # brand = item.find('span', class_='a-size-medium a-color-base').text if item.find('span', class_='a-size-medium a-color-base') else 'None'
             url_product = f'https://www.amazon.com{a_tag.get("href")}'
             thumbnail_link = item.find('img', class_='s-image').get('src')
-            rating = item.find('span', class_='a-icon-alt').text.replace(' out of 5 stars', '') if item.find('span', class_='a-icon-alt') else 'None'
-            price_whole = item.find('span', class_='a-price-whole').text if item.find('span', class_='a-price-whole') else None
+            rating = float(item.find('span', class_='a-icon-alt').text.replace(' out of 5 stars', '')) if item.find('span', class_='a-icon-alt') else None
+            price_whole = item.find('span', class_='a-price-whole').text.replace(',', '') if item.find('span', class_='a-price-whole') else None
             price_fraction = item.find('span', class_='a-price-fraction').text if item.find('span', class_='a-price-fraction') else None
-            product_price = f'{price_whole}{price_fraction}' if price_whole and price_fraction else 'None'
+            product_price = float(f'{price_whole}{price_fraction}') if price_whole and price_fraction else None
 
             product = {
                 'title': title,
                 'thumbnail_link': thumbnail_link,
                 'url': url_product,
                 'rating': rating,
-                'price': product_price
+                'price_in_usd': product_price
             }
 
             results.append(product)
@@ -77,6 +84,35 @@ class AmazonScraper:
         
         wb.save(file_name)
 
+
+    def input_to_postgresdb(self, parsed_products):
+        try:
+            global engine
+
+            POSTGRES_USER = os.environ.get('POSTGRES_USER')
+            POSTGRES_DB = os.environ.get('POSTGRES_DB')
+            POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+            engine = engine.create_engine(f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}')
+
+            global conn
+            conn = engine.connect()
+            
+            query = text('CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, title VARCHAR(500), thumbnail_link VARCHAR(500), url VARCHAR(500), rating NUMERIC(2,1), price_in_usd NUMERIC(10,2));') 
+            conn.execute(query)
+            conn.commit()
+    
+            for item in parsed_products:
+                stmt = text("INSERT INTO products (title, thumbnail_link, url, rating, price_in_usd) VALUES (:title, :thumbnail_link, :url, :rating, :price_in_usd)")
+                conn.execute(stmt, item)
+            
+            conn.commit()
+            
+            conn.close()
+
+        except Exception as e:
+            print("Error while connecting to PostgreSQL:", e)
+            
 
     def run(self, keywords, file_name):
 
@@ -115,6 +151,7 @@ class AmazonScraper:
     
             parsed_products = self.parsing_product_item(result)
             self.input_to_xlsx(parsed_products, file_name)
+            self.input_to_postgresdb(parsed_products)
 
 
 
